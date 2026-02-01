b96ff6528f08233ec9acef38cd74dee99f8a6a94
kavi-234 <kprabuddhi2001@gmail.com>
2026-02-01 22:53:17 +0530

    update

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { OAuth2Client } from 'google-auth-library';
-import User from '../models/User.js';
+import User from '../models/user.js';
 
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
